92ba14d9fc9a5521418b1e19e3ded80fbaf68528
razvanpotoceanu <razvanpotoceanu4@gmail.com>
2025-10-19 17:05:26 +0300

    Aufgabe4 - BilligsteTastatur + test in main

---
+++ src/aufgabe4/Main.java
@@ -0,0 +1,15 @@
+package aufgabe4;
+
+public class Main {
+    public static void main(String[] args) {
+        //aufgabe1
+
+        int[] tastaturen = {40,35, 70, 15, 45, 10};
+        System.out.println("Die billigste Tastatur: "+ BilligsteTastatur.findeBilligsteTastatur(tastaturen));
+
+
+        //aufgabe2
+
+
+    }
+}
